d0fef0f3e37202a14232bc9a3d74ccd3374c3240
Fabio Luiz <fabio.fonseca@souunit.com.br>
2026-01-07 19:10:31 -0300

    Fundamentos
    aprendendo Kotlin seus fundamentos, atributos, variáveis com Spring.

---
+++ bookXis/src/main/kotlin/com/bookXis/bookXis/BookXisApplication.kt
@@ -0,0 +1,11 @@
+package com.bookXis.bookXis
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BookXisApplication
+
+fun main(args: Array<String>) {
+	runApplication<BookXisApplication>(*args)
+}
---
+++ bookXis/src/test/kotlin/com/bookXis/bookXis/BookXisApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bookXis.bookXis
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookXisApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
