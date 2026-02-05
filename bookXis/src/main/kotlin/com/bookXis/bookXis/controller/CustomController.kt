62571a2c2781ff9bc70c6d99d2d4329f586a8cdd
Fabio Luiz <fabio.fonseca@souunit.com.br>
2026-02-05 13:10:40 -0300

    Request e Post
    Foi criado a pasta request como metodo de recebimento de informações do usuario na aplicação que recebera as informações pela classe PostCustomRequest, enviada pelo usuario no post da aplicação, é não ira tentar receber o id mais do usuario pois é o identificador do sistema e não ha necessidade do usuario poder mecher nela, recebendo apenas o que nos queremos da aplicação do usuario no caso o nome é o email. Sendo assim esse padrão é usado para não ser recebido todo o nosso model permitindo receber só as informações que fazem sentindo de receber naquele endpoint.

--- bookXis/src/main/kotlin/com/bookXis/bookXis/controller/CustomController.kt
+++ bookXis/src/main/kotlin/com/bookXis/bookXis/controller/CustomController.kt
@@ -1,11 +1,14 @@
 package com.bookXis.bookXis.controller
 
+import com.bookXis.bookXis.controller.request.PostCustomRequest
 import com.bookXis.bookXis.model.CustomModel
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RequestMapping("/books")
+@RequestMapping("books") //variavel
 @RestController
 
 class CustomController {
@@ -13,6 +16,11 @@ class CustomController {
     @GetMapping
     fun getCustomer(): CustomModel {
         return CustomModel("1","Gustavo","email@gmail.com")
+    }
+
+    @PostMapping
+    fun create(@RequestBody books: PostCustomRequest) {
+        println(books)
     }
 
 }
---
+++ bookXis/src/main/kotlin/com/bookXis/bookXis/controller/request/PostCustomRequest.kt
@@ -0,0 +1,8 @@
+package com.bookXis.bookXis.controller.request
+
+data class PostCustomRequest (
+
+    var name: String,
+
+    var email: String,
+)
